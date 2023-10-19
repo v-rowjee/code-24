dd98f1a2b933924fdc85fd0898032ba37530c4fe
Ved Rowjee <84242585+v-rowjee@users.noreply.github.com>
2023-10-19 10:34:28 +0400

    bootstrap

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import {Button} from 'react-bootstrap';
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,16 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button variant="primary">Primary</Button>
       </header>
     </div>
   );
