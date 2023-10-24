305b72d14031f91bb50afb2ba891ec8f274a7d20
a.aisha <auckburallyaisha@gmail.com>
2023-10-24 19:38:01 +0400

    servi tuzur

---
+++ src/AppBar.js
@@ -0,0 +1,130 @@
+import React from "react";
+import {
+  Box,
+  AppBar,
+  Drawer,
+  CssBaseline,
+  Toolbar,
+  List,
+  Typography,
+  Divider,
+  IconButton,
+  // Link,
+  ListItem,
+  ListItemButton,
+  // ListItemIcon,
+  ListItemText,
+  Button,
+} from "@mui/material";
+import { Menu, Share } from "@mui/icons-material";
+import colours from "./colours";
+
+const drawerWidth = 240;
+const navItems = [
+  "Share",
+  "Summary",
+  "Keypoint",
+  "Action Item",
+  "Sentiment Analysis",
+  "Meeting Insight",
+];
+
+const DrawerAppBar = () => {
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        MEETING
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton sx={{ textAlign: "center" }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar
+        component="nav"
+        style={{ height: "64px", backgroundColor: colours.primaryOrange }}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: "none" } }}
+          >
+            <Menu />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+          >
+            MEETING
+          </Typography>
+          <Box sx={{ display: { xs: "none", sm: "block" } }}>
+            <Button
+              variant="outlined"
+              sx={{
+                color: colours.lightText,
+                borderColor: colours.lightText,
+              }}
+              startIcon={<Share />}
+            >
+              Share
+            </Button>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <nav>
+        <Drawer
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </nav>
+      <Box
+        component="main"
+        sx={{
+          p: 3,
+          backgroundColor: colours.background,
+          flex: 1,
+          height: "100vh",
+        }}
+      >
+        <Toolbar />
+        <Typography>Hello World.</Typography>
+      </Box>
+    </Box>
+  );
+};
+
+export default DrawerAppBar;
---
+++ src/colours.js
@@ -0,0 +1,12 @@
+//define colour palette
+
+const Colors = {
+  background: "#F5F6F7", // screen background
+  lightText: "#E3E7EC", // light text and icons
+  darkText: "#434241", // dark text and icons
+  cardText: "#8A93B0", // card text
+  primaryOrange: "#EA985A", //app bar and buttons
+  secondaryOrange: "#F5DFCD", // side bar and button shadow
+};
+
+export default Colors;
